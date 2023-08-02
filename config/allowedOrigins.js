81edb2e9bac6b2fd64a996621c338d00015667c9
ChrisMKocabas <muhammedkocabas@outlook.com>
2023-08-02 15:36:20 -0400

    cors update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "74.12.25.70:5173",
-  "http://localhost:5173",
+  "https://todo-app-fr.onrender.com",
+  "https://todo-app-fr.onrender.com:*",
   "*",
 ];
 
